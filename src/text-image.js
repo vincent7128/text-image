93f56c4f200daee94e42bbcb7e9a9ffa5c26f2e6
Anuj Sharma <anuj9196@users.noreply.github.com>
2019-03-01 17:49:02 +0530

    Add support for bold and italic styles

--- src/text-image.js
+++ src/text-image.js
@@ -10,7 +10,9 @@
             background: 'rgba(0, 0, 0, 0)',
             stroke: 0,
             strokeColor: '#FFFFFF',
-            lineHeight: '1.2em'
+            lineHeight: '1.2em',
+            bold: false,
+            italic: false
         },
         preStyle = ';padding: 0; display: block; position: fixed; top: 100%; overflow: hidden;',
         fn;
@@ -32,7 +34,14 @@
                 this.style[key] = _style[key];
             }
         }
-        this._style = 'font: ' + this.style.size + 'pt ' + this.style.font + ';';
+        this._style = 'font: ';
+        if (this.style.italic) {
+            this._style += 'italic ';
+        }
+        if (this.style.bold) {
+            this._style += 'bold ';
+        }
+        this._style += this.style.size + 'pt ' + this.style.font + ';';
         this._style += 'line-height:' + this.style.lineHeight + ';';
         this._style += 'text-align: ' + this.style.align + ';';
         this._style += 'color: ' + this.style.color + ';';
